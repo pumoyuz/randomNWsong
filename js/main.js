a4a3351a2bfe74ee82713dd4cf32ef2bcad51e7a
k_fuseya <k_fuseya@alta.co.jp>
2020-06-05 10:51:59 +0900

    init

---
+++ js/main.js
@@ -0,0 +1,271 @@
+// 曲名を配列にセット
+// シングル
+var newsNippon = ['NEWSニッポン','ありがとう・今','Private Hearts'];
+var yell = ['希望～Yell～','Stand Up','Good News!','LET’S GO TO THE PLANETS'];
+var akaku = ['紅く燃ゆる太陽','忘れないさ～LIFE GOES ON～','DREAMS','BEACH ANGEL'];
+var cherish = ['チェリッシュ','Party Time','SHOCK ME','Devil or Angel'];
+var teppen = ['TEPPEN','夢の数だけ愛が生まれる','NANDE×2 DAME','Fiesta'];
+var sayaHadashi = ['サヤエンドウ','裸足のシンデレラボーイ'];
+var hoshimeza = ['星をめざして','Boom! Boom! POWER','紅い花','Best Friend'];
+var weeeek = ['weeeek','with me','Why','Rainbow'];
+var taiyono = ['太陽のナミダ','美しすぎて Beautiful Eyes','バンビーナ','Lady Spider'];
+var summerTime = ['SUMMER TIME','EASY COME, EASY GO','Liar','Baby! Be My Baby!'];
+var happyBirthday = ['Happy Birthday','ガンガンガンバッテ','GAME of LOVE','Push On!'];
+var abo = ['恋のABO','ラビリンス','OPEN YOUR EYES','Share（Live at TOKYO DOME）'];
+var sakuraGirl = ['さくらガール','あなたがとなりにいるだけで','Love Melodies','FREEDOM','さくらガール（A cappella version）'];
+var fighting = ['Fighting Man','ガムシャラ Cha Cha Cha','Wake Up','Winter Moon','愛はシンプルなカレーライス'];
+var chanka = ['チャンカパーナ','フルスイング','Starry','ヴァンパイアはかく語りき','PeekaBoo...','Addict'];
+var worldPoko = ['WORLD QUEST','ポコポンペコーリャ','Hello','Quntastic!','36℃'];
+var one = ['ONE -for the win-','SEVEN COLORS','君がいた夏','FLYING BIRD'];
+var kaguya = ['KAGUYA','バタフライ','TRAVeLiNG','勿忘草','TOP OF THE WORLD'];
+var cuum = ['チュムチュム','日はまた昇る','メガロマニア','Sweet Martini','ささぶね'];
+var yonjushi = ['四銃士','ANTHEM','永遠','SPEAKER'];
+var hikariTouch = ['ヒカリノシズク','Touch','星の旅人たち','whis･per'];
+var koishira = ['恋を知らない君へ','Smile','サマラバ','Distance'];
+var emma = ['EMMA','Snow Dance','スノードロップ','さくらガール -Represent NEWS Mix-','I･ZA･NA･I･ZU･KI -Represent NEWS Mix-'];
+var lps = ['LPS','NEWSICAL','madoromi','チェリッシュ -Represent NEWS Mix-','真冬のナガレボシ -Represent NEWS Mix-'];
+var blue = ['BLUE','Cascade','夜よ踊れ','NEWSニッポン -Represent NEWS Mix-','Stand Up -Represent NEWS Mix-'];
+var ikiro = ['｢生きろ｣','Bring Back the Summer','LVE','希望～Yell～ -Represent NEWS Mix-','エンドレス・サマー -Represent NEWS Mix-'];
+var topLove = ['トップガン','Love Story','Dragonism','weeeek -Represent NEWS Mix-','夢の数だけ愛が生まれる -Represent NEWS Mix-'];
+// アルバム
+var touch = ['希望～Yell～','きらめきの彼方へ','紅く燃ゆる太陽','I・ZA・NA・I・ZU・KI','LOVE SONG','ずっと','High TEN!','恋焼け','疾走！Friday Night','Say Hello','チェリッシュ','柔らかなままで','NEWSニッポン','夢の数だけ愛が生まれる<A cappella version＞','Stand Up＜Rock version＞','チェリッシュ＜Ryoji（from ケツメイシ）Remix＞'];
+var pacific = ['愛のマタドール','サヤエンドウ','TEPPEN','Change the World','君想フ夜','アリバイ','code','チラリズム','愛なんて','なんとかなるさ','ゴメンネ ジュリエット','裸足のシンデレラボーイ','星をめざして','真冬のナガレボシ','その笑顔 僕に見せて'];
+var color = ['weeeek','STARDUST','SUMMER TIME','SNOW EXPRESS','Forever','MOLA','ケセナイ','ordinary','みんながいる世界をひとつに愛をもっとGive & Takeしましょう','ムラリスト','太陽のナミダ','Smile Maker','Happy Birthday','FLY AGAIN','永遠色の恋'];
+var live = ['恋のABO','LIVE','生まれし君へ','Supernatural','秋の空','2人/130000000の奇跡','Dancin’ in the Secret','ワンダーランド','さくらガール','BE FUNKY!','D.T.F','内容の無い手紙','エンドレス・サマー','Share','Forever（Unplugged Ver.）'];
+var newsBest = ['NEWSニッポン','希望～Yell～','紅く燃ゆる太陽','チェリッシュ','TEPPEN','サヤエンドウ','裸足のシンデレラボーイ','星をめざして','weeeek','太陽のナミダ','SUMMER TIME','Happy Birthday','恋のABO','さくらガール','Fighting Man','エンドレス・サマー','Share','I・ZA・NA・I・ZU・KI','SNOW EXPRESS','DREAMS','FLY AGAIN','Smile Maker','Forever','バンビーナ','きらめきの彼方へ','夢の数だけ愛が生まれる','2人/130000000の奇跡','真冬のナガレボシ','チェリッシュ ＜Ryoji（from ケツメイシ）Remix＞','永遠色の恋','Love Addiction','Private Hearts','Uri Sarang','暁-AKATSUKI-','Pumpkin','SUPERMAN','HAPPY MUSIC','カカオ','シャララタンバリン','ごみ箱','Stars','愛なんて'];
+var newsAlbum = ['WORLD QUEST','4+FAN','渚のお姉サマー','ポコポンペコーリャ','恋祭り','Greedier','ベサメ･ムーチョ～狂おしいボレロ～','チャンカパーナ','Dance in the dark','HIGHER GROUND','フルスイング','CRY','Beautiful Rain','Dreamcatcher','Remedy','Lovin’ U'];
+var white = ['MR.WHITE','KAGUYA','NYARO','SEVEN COLORS','Weather NEWS','SuperSONIC','BYAKUYA','ONE -for the win-','White Love Story','愛言葉','ロメオ 2015','Skye Beautiful','あなた','ESCORT'];
+var quartetto = ['QUARTETTO','ANTHEM','シリウス','Touch','NEWSKOOL','四銃士','Wonder','ライフ','チュムチュム','Departure','ヒカリノシズク','LIS’N','愛のエレジー','星の王子さま','Encore'];
+var neverland = ['NEVERLAND','アン･ドゥ･トロワ','EMMA','Brightest','Silent Love','恋を知らない君へ','ミステリア','BLACK FIRE','ORIHIME','流れ星','U R not alone','I’m coming','ニャン太','あやめ','FOREVER MINE'];
+var epcotia = ['EPCOTIA','KINGDOM','TWINKLE STAR','LPS','恋する惑星','JUMP AROUND','AVALON','IT’S YOU','UFO','EROTICA','BLACKHOLE','星に願いを','イノセンス','HAPPY ENDING','プラトニック','銀座ラプソディ','氷温','Thunder'];
+var worldista = ['WORLDISTA','DEAD END','CASINO DRIVE','インビジブル ダンジョン','SPIRIT','BLUE','FIGHTERS.COM','Digital Love','リボン','サンタのいないクリスマス','Strawberry','｢生きろ｣','Symphony of Dissonance','DoLLs','Going that way','世界'];
+var story = ['STORY','SEVEN','SUPERSTAR','何度でも','STAY WITH ME','Perfect Lover','Love Story','エス','トップガン','君の言葉に笑みを','クローバー','NEW STORY','戀','Narrative','STAY ALIVE','プロポーズ'];
+// ライブ
+var biren = ['チャンカパーナ','NEWSニッポン','weeeek','Happy Birthday','Fighting Man','恋のABO','BE FUNKY!','BEACH ANGEL','SUMMER TIME','LET’S GO TO THE PLANETS','愛はシンプルなカレーライス','あなたがとなりにいるだけで','紅い花','星をめざして','DREAMS','希望～Yell～','きらめきの彼方へ','サヤエンドウ','希望～Yell～','裸足のシンデレラボーイ','TEPPEN','Addict','ヴァンパイアはかく語りき','エンドレス･サマー','Share','I･ZA･NA･I･ZU･KI','チェリッシュ','PeekaBoo...','Starry','バンビーナ','Dancin’ in the Secret','紅く燃ゆる太陽','Smile Maker','2人/130000000の奇跡','フルスイング','さくらガール','NEWSニッポン','チャンカパーナ','weeeek','チャンカパーナ'];
+var tenTh = ['WORLD QUEST','チャンカパーナ','4+FAN','渚のお姉サマー','恋祭り','フルスイング','Good News!','NEWSニッポン','希望～Yell～','太陽のナミダ','ポコポンペコーリャ','エンドレス･サマー','Greedier','Remedy','SHOCK ME 2013','BE FUNKY!','Beautiful Rain','2人/130000000の奇跡','SUMMER TIME','チェリッシュ','Dreamcatcher','Lovin’U','Liar','ベサメ･ムーチョ～狂おしいボレロ～','Dance in the dark','Fighting Man','紅く燃ゆる太陽','TEPPEN','サヤエンドウ','裸足のシンデレラボーイ','星をめざして','さくらガール','恋のABO','weeeek','HIGHER GROUND','CRY','Happy Birthday','4+FAN','恋祭り','愛言葉～てをひいて～'];
+var whiteLive = ['MR.WHITE','ONE -for the win-','恋のABO','NYARO','weeeek','恋祭り','バタフライ','フルスイング','ロメオ 2015','ESCORT','KAGUYA','チャンカパーナ','WORLD QUEST','さくらガール','Weather NEWS','勿忘草','あなた','Skye Beautiful','BYAKUYA','SuperSONIC','SNOW EXPRESS','Winter Moon','NEWSニッポン','希望～Yell～','サヤエンドウ','ポコポンペコーリャ','4+FAN','SEVEN COLORS','White Love Story','渚のお姉サマー','愛言葉','チュムチュム','NYARO'];
+var quartettoLive = ['QUARTETTO','ANTHEM','チャンカパーナ','希望～Yell～','チュムチュム','KAGUYA','太陽のナミダ','Happy Birthday','チェリッシュ','星の王子さま','LIS’N','Touch','NEWSKOOL','weeeek','Departure','愛言葉','四銃士','星をめざして','愛のエレジー','Encore','I･ZA･NA･I･ZU･KI','Wonder','シリウス','NEWSニッポン','ライフ','サヤエンドウ','恋のABO','TEPPEN','NYARO','ヒカリノシズク','ONE -for the win-','さくらガール','恋祭り','weeeek'];
+var neverlandLive = ['NEVERLAND','アン･ドゥ･トロワ','EMMA','KAGUYA','恋祭り','D.T.F','4+FAN','あやめ','Brightest','シリウス','Snow Dance','スノードロップ','Touch','ニャン太','恋を知らない君へ','フルスイング','恋のABO','サマラバ','NYARO','ORIHIME','FOREVER MINE','Silent Love','ミステリア','BYAKUYA','さくらガール','I’m coming','BLACK FIRE','バンビーナ','ANTHEM','チャンカパーナ','チュムチュム','渚のお姉サマー','weeeek','ポコポンペコーリャ','流れ星','U R not alone'];
+var epcotiaLive = ['EPCOTIA','KINGDOM','TWINKLE STAR','紅く燃ゆる太陽','LIVE','LPS','NYARO','恋する惑星','銀座ラプソディ','Sweet Martini','madoromi','チャンカパーナ','JUMP AROUND','BLACKHOLE','Thunder','氷温','AVALON','IT’S YOU','星に願いを','プラトニック','UFO','EMMA','EROTICA','メガロマニア','4＋FAN','D.T.F','weeeek','U R not alone','イノセンス','HAPPY ENDING'];
+var strawberry = ['BLUE','恋祭り','チュムチュム','サマラバ','BEACH ANGEL','恋のABO','weeeek','Happy Birthday','愛言葉','チャンカパーナ','SUMMER TIME','フルスイング','U R not alone','青いベンチ','Share','Touch','エンドレス･サマー','ヒカリノシズク','夜よ踊れ','JUMP AROUND','紅く燃ゆる太陽','BLACK FIRE','NEWSニッポン','渚のお姉サマー','NYARO','星をめざして','チェリッシュ','4＋FAN','Stand Up','希望～Yell～','SHOCK ME 2013','KAGUYA','サヤエンドウ','裸足のシンデレラボーイ','BYAKUYA','さくらガール','Fighting Man','LPS','｢生きろ｣'];
+var epcotiaEnc = ['EPCOTIA','TWINKLE STAR','U R not alone','SPIRIT','紅く燃ゆる太陽','Stand Up','NEWSニッポン','LIVE','weeeek','恋する惑星','IT’S YOU','SHOCK ME 2013','チャンカパーナ','夜よ踊れ','LVE','JUMP AROUND','BLACK FIRE','Strawberry','ずっと一緒さ','DANCIN’☆TO ME','カカオ','茜空','Cascade','AVALON','EROTICA','UFO','メガロマニア','BLACKHOLE','LPS','恋祭り','NYARO','渚のお姉サマー','EMMA','チュムチュム','KAGUYA','Fighting Man','裸足のシンデレラボーイ','TEPPEN','希望～Yell～','4＋FAN','｢生きろ｣','HAPPY ENDING','BLUE'];
+// ソロ
+var koya = ['Love Addiction','Private Hearts','Uri Sarang','Starry','Beautiful Rain','ロメオ 2015','愛のエレジー','ニャン太','銀座ラプソディ','Going that way','STAY ALIVE'];
+var shige = ['HAPPY MUSIC','シャララタンバリン','カカオ','ヴァンパイアはかく語りき','Dreamcatcher','ESCORT','星の王子さま','あやめ','氷温','世界','Narrative'];
+var mass = ['Pumpkin','SUPERMAN','暁-AKATSUKI-','PeekaBoo...','Remedy','Skye Beautiful','LIS’N','FOREVER MINE','Thunder','Symphony of Dissonance','戀'];
+var tego = ['Stars','ごみ箱','Addict','Lovin’ U','あなた','Encore','I’m coming','プラトニック','DoLLs','プロポーズ'];
+
+// 全選択ボタンクリック時の挙動
+function clickAllCheck() {
+for (var i = 0; i < document.settingForm.songs.length; i++){
+      document.settingForm.songs[i].checked = true;
+  }
+}
+// 全解除ボタンクリック時の挙動
+function clickAllClear() {
+for (var i = 0; i < document.settingForm.songs.length; i++){
+      document.settingForm.songs[i].checked = false;
+  }
+}
+
+// Chooseボタンクリック時の挙動
+function ChooseButtonClick() {
+  var songs = [];
+  var checkCount = [];
+  var quantity = 0;
+  var songsResult = "";
+  var flag = false;
+
+  // 抽出数確認
+  var quantity = document.getElementById("js-quantity").value;
+  var quantityNo = parseInt(quantity);
+  if (quantityNo > 101) {
+    alert("曲数は1~100の間で設定してください。");
+    return false;
+  }
+
+  // チェックボックス確認
+  for (var i = 0; i < document.settingForm.length; i++){
+    if (document.settingForm.songs[i].checked) {
+    flag = true;    
+    //console.log(document.settingForm.songs[i].value + "が選択されました。");
+    var checkCount = checkCount.concat(document.settingForm.songs[i].value);
+    }
+  }
+
+  // 配列に曲名を追加
+  // シングル
+  if (checkCount.indexOf("newsNippon") >= 0) {
+    var songs = songs.concat(newsNippon);
+  }
+  if (checkCount.indexOf("yell") >= 0) {
+    var songs = songs.concat(yell);
+  }
+  if (checkCount.indexOf("akaku") >= 0) {
+    var songs = songs.concat(akaku);
+  }
+  if (checkCount.indexOf("cherish") >= 0) {
+    var songs = songs.concat(cherish);
+  }
+  if (checkCount.indexOf("teppen") >= 0) {
+    var songs = songs.concat(teppen);
+  }
+  if (checkCount.indexOf("sayaHadashi") >= 0) {
+    var songs = songs.concat(sayaHadashi);
+  }
+  if (checkCount.indexOf("hoshimeza") >= 0) {
+    var songs = songs.concat(hoshimeza);
+  }
+  if (checkCount.indexOf("weeeek") >= 0) {
+    var songs = songs.concat(weeeek);
+  }
+  if (checkCount.indexOf("taiyono") >= 0) {
+    var songs = songs.concat(taiyono);
+  }
+  if (checkCount.indexOf("summerTime") >= 0) {
+    var songs = songs.concat(summerTime);
+  }
+  if (checkCount.indexOf("happyBirthday") >= 0) {
+    var songs = songs.concat(happyBirthday);
+  }
+  if (checkCount.indexOf("abo") >= 0) {
+    var songs = songs.concat(abo);
+  }
+  if (checkCount.indexOf("sakuraGirl") >= 0) {
+    var songs = songs.concat(sakuraGirl);
+  }
+  if (checkCount.indexOf("fighting") >= 0) {
+    var songs = songs.concat(fighting);
+  }
+  if (checkCount.indexOf("chanka") >= 0) {
+    var songs = songs.concat(chanka);
+  }
+  if (checkCount.indexOf("worldPoko") >= 0) {
+    var songs = songs.concat(worldPoko);
+  }
+  if (checkCount.indexOf("one") >= 0) {
+    var songs = songs.concat(one);
+  }
+  if (checkCount.indexOf("kaguya") >= 0) {
+    var songs = songs.concat(kaguya);
+  }
+  if (checkCount.indexOf("cuum") >= 0) {
+    var songs = songs.concat(cuum);
+  }
+  if (checkCount.indexOf("yonjushi") >= 0) {
+    var songs = songs.concat(yonjushi);
+  }
+  if (checkCount.indexOf("hikariTouch") >= 0) {
+    var songs = songs.concat(hikariTouch);
+  }
+  if (checkCount.indexOf("koishira") >= 0) {
+    var songs = songs.concat(koishira);
+  }
+  if (checkCount.indexOf("emma") >= 0) {
+    var songs = songs.concat(emma);
+  }
+  if (checkCount.indexOf("lps") >= 0) {
+    var songs = songs.concat(lps);
+  }
+  if (checkCount.indexOf("blue") >= 0) {
+    var songs = songs.concat(blue);
+  }
+  if (checkCount.indexOf("ikiro") >= 0) {
+    var songs = songs.concat(ikiro);
+  }
+  if (checkCount.indexOf("topLove") >= 0) {
+    var songs = songs.concat(topLove);
+  }
+  // アルバム
+  if (checkCount.indexOf("touch") >= 0) {
+    var songs = songs.concat(touch);
+  }
+  if (checkCount.indexOf("pacific") >= 0) {
+    var songs = songs.concat(pacific);
+  }
+  if (checkCount.indexOf("color") >= 0) {
+    var songs = songs.concat(color);
+  }
+  if (checkCount.indexOf("live") >= 0) {
+    var songs = songs.concat(live);
+  }
+  if (checkCount.indexOf("newsBest") >= 0) {
+    var songs = songs.concat(newsBest);
+  }
+  if (checkCount.indexOf("newsAlbum") >= 0) {
+    var songs = songs.concat(newsAlbum);
+  }
+  if (checkCount.indexOf("white") >= 0) {
+    var songs = songs.concat(white);
+  }
+  if (checkCount.indexOf("quartetto") >= 0) {
+    var songs = songs.concat(quartetto);
+  }
+  if (checkCount.indexOf("neverland") >= 0) {
+    var songs = songs.concat(neverland);
+  }
+  if (checkCount.indexOf("epcotia") >= 0) {
+    var songs = songs.concat(epcotia);
+  }
+  if (checkCount.indexOf("worldista") >= 0) {
+    var songs = songs.concat(worldista);
+  }
+  if (checkCount.indexOf("story") >= 0) {
+    var songs = songs.concat(story);
+  }
+  // ライブ
+  if (checkCount.indexOf("biren") >= 0) {
+    var songs = songs.concat(biren);
+  }
+  if (checkCount.indexOf("tenTh") >= 0) {
+    var songs = songs.concat(tenTh);
+  }
+  if (checkCount.indexOf("whiteLive") >= 0) {
+    var songs = songs.concat(whiteLive);
+  }
+  if (checkCount.indexOf("quartettoLive") >= 0) {
+    var songs = songs.concat(quartettoLive);
+  }
+  if (checkCount.indexOf("neverlandLive") >= 0) {
+    var songs = songs.concat(neverlandLive);
+  }
+  if (checkCount.indexOf("epcotiaLive") >= 0) {
+    var songs = songs.concat(epcotiaLive);
+  }
+  if (checkCount.indexOf("strawberry") >= 0) {
+    var songs = songs.concat(strawberry);
+  }
+  if (checkCount.indexOf("epcotiaEnc") >= 0) {
+    var songs = songs.concat(epcotiaEnc);
+  }
+  // ソロ
+  if (checkCount.indexOf("koya") >= 0) {
+    var songs = songs.concat(koya);
+  }
+  if (checkCount.indexOf("shige") >= 0) {
+    var songs = songs.concat(shige);
+  }
+  if (checkCount.indexOf("mass") >= 0) {
+    var songs = songs.concat(mass);
+  }
+  if (checkCount.indexOf("tego") >= 0) {
+    var songs = songs.concat(tego);
+  }
+
+if (flag > 0) {
+  for (var step = 0; step < quantityNo; step++) {
+    target = document.getElementById("js-result-output");
+    var songNo = Math.floor(Math.random() * songs.length);
+    songsResult = songsResult + "<p>" + songs[songNo] + "</p>";
+    target.innerHTML = songsResult;
+    //return false;
+  }
+}
+
+
+
+// 何も選択されていない場合の処理   
+if (!flag) {
+  alert("曲を選択してください。");
+}
+}
